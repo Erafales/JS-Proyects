b94db1ae084f4a6228ee3140e4e18405a7fc02b3
eric rafales <alumno9maella@gmail.com>
2024-05-25 09:21:26 +0200

    add some JS loggic

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
+var num1;
+var num2;
+var operador;
+var resultado;
+
 function ObtenerNumero(numero){
-    console.log(numero);
+    if (num1 == null) {
+        num1 = numero;
+        console.log('Valor num1:' + num1);
+    } else {
+        num2 = numero;
+        console.log('Valor num2:' + num2);
+    }
+    Magic(num1, num2, operador);
 }
-function ObtenerOperador(operador){
+function ObtenerOperador(sign){
+    console.log(sign);
+    if (sign == "="){
+        document.getElementById('print-result').innerText=(resultado);
+    } else {
+        operador = sign;
+        console.log('Valor operador:' + operador);
+    }
+}
+
+function Magic(num1, num2, operador){
     console.log(operador);
+    switch (operador) {
+        case '+': 
+            resultado = num1 + num2;
+            console.log(resultado);
+              C();
+            break;
+        case '-': 
+            resultado = num1 - num2;
+            console.log(resultado);
+            C();
+            break;
+    }
+}
+function C(){
+    if (num1 != null && num2 != null){
+        num1 = null;
+        num2 = null;
+    } else {}
 }

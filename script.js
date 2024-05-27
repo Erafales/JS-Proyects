2783fdf7bb92fe99b15262a0a425798444026ef8
eric rafales <alumno9maella@gmail.com>
2024-05-27 21:58:42 +0200

    New branch

--- script.js
+++ script.js
@@ -1,46 +1,20 @@
-var num1;
-var num2;
-var operador;
-var resultado;
 
-function ObtenerNumero(numero){
-    if (num1 == null) {
-        num1 = numero;
-        console.log('Valor num1:' + num1);
-    } else {
-        num2 = numero;
-        console.log('Valor num2:' + num2);
-    }
-    Magic(num1, num2, operador);
-}
-function ObtenerOperador(sign){
-    console.log(sign);
-    if (sign == "="){
-        document.getElementById('print-result').innerText=(resultado);
-    } else {
-        operador = sign;
-        console.log('Valor operador:' + operador);
-    }
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
 }
 
-function Magic(num1, num2, operador){
-    console.log(operador);
-    switch (operador) {
-        case '+': 
-            resultado = num1 + num2;
-            console.log(resultado);
-              C();
-            break;
-        case '-': 
-            resultado = num1 - num2;
-            console.log(resultado);
-            C();
-            break;
-    }
+function clearDisplay(){
+ display.value = "";
 }
-function C(){
-    if (num1 != null && num2 != null){
-        num1 = null;
-        num2 = null;
-    } else {}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Syntax Error";
+    }
 }

4908c5569f3edea6f8af1bdf5d161a790646f840
eric rafales <alumno9maella@gmail.com>
2024-07-05 15:38:23 +0200

    Added social links and ToDo on script.js

---
+++ Web-Personal/script.js
@@ -0,0 +1,6 @@
+/*
+    Tenemos que añadir:
+    1. Al darle click al botón de Twitter => Mensaje de que no tenemos twitcer
+    2. Al pulsar sobre "Contáctame" => Dirección de correo electronico
+    3. Hablemos igual que punto 2
+*/
